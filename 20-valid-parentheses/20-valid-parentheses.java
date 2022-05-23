986d63dd745743aa7979477d7000f94d1508e4a5
shima osamah <60664444+shimaOSamah@users.noreply.github.com>
2022-05-23 22:49:18 +0300

    Time: 10 ms (5.20%), Space: 40.4 MB (84.31%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean isValid(String s) {
+        
+        Stack <Character> st = new Stack <Character>();
+        
+        if(s.length() % 2 != 0)
+            return false;
+        
+        for(char c : s.toCharArray()){
+            
+            if(c == '(' || c == '[' || c == '{'){
+                st.push(c);
+            }
+            
+            else if(c == ')' || c == ']' || c == '}'){
+                
+                if(st.empty())
+                    return false;
+                
+                if(c - st.peek() <= 2 && c - st.peek() > 0){
+                    System.out.println(c - st.peek());
+                    st.pop();
+                }
+                
+                else{
+                    return false;
+                }
+            }
+        }
+        
+        return st.empty();
+        
+    }
+}
